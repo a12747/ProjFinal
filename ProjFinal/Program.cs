ba51240ebb6571c36b48b4ac9d4760e830b7bb6a
João Carvalho <a12747@alunos.ipca.pt>
2025-03-17 15:34:33 +0000

    Added changes to main program file and updated nuggets to latest version

--- ProjFinal/Program.cs
+++ ProjFinal/Program.cs
@@ -1,9 +1,121 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+#region Swagger
+
+// Configure Swagger for API documentation with JWT security
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Prediction API",
+            Version = "v1",
+            Description = "API Documentation",
+            Contact = new OpenApiContact
+            {
+                Name = "João Carvalho",
+                Email = "a12747@alunos.ipca.pt"
+            }
+        }
+    );
+
+    // JWT Bearer configuration for Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Insert JWT token in 'Authorization' field. Format: Bearer {token}",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
+    // Includes XML comments in Swagger
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+#endregion
+
+#region Authentication
+
+// JWT
+builder.Configuration["JwtSettings:SecretKey"] = Environment.GetEnvironmentVariable("JWT_SECRET_KEY") ??
+                                                 builder.Configuration["JwtSettings:SecretKey"];
+builder.Configuration["JwtSettings:Issuer"] = Environment.GetEnvironmentVariable("JWT_ISSUER") ??
+                                              builder.Configuration["JwtSettings:Issuer"];
+builder.Configuration["JwtSettings:Audience"] = Environment.GetEnvironmentVariable("JWT_AUDIENCE") ??
+                                                builder.Configuration["JwtSettings:Audience"];
+builder.Configuration["JwtSettings:Expiration"] = Environment.GetEnvironmentVariable("JWT_EXPIRATION_MINUTES") ??
+                                                  builder.Configuration["JwtSettings:Expiration"];
+
+// Configure JWT authentication with the required parameters
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    var secretKey = builder.Configuration["JwtSettings:SecretKey"] ??
+                    throw new ArgumentNullException("JwtSettings:SecretKey is missing.");
+    var issuer = builder.Configuration["JwtSettings:Issuer"] ??
+                 throw new ArgumentNullException("JwtSettings:Issuer is missing.");
+    var audience = builder.Configuration["JwtSettings:Audience"] ??
+                   throw new ArgumentNullException("JwtSettings:Audience is missing.");
+    var key = Encoding.UTF8.GetBytes(secretKey);
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+
+    options.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = context =>
+        {
+            Console.WriteLine($"Authentication failed: {context.Exception.Message}");
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
+            Console.WriteLine("Token validated successfully.");
+            return Task.CompletedTask;
+        }
+    };
+});
+
+#endregion
+
 
 var app = builder.Build();
 
@@ -15,30 +127,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseAuthentication();
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.UseExceptionHandler("/error");
+app.Map("/error", (HttpContext context) => { return Results.Problem("An internal error occurred."); });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
